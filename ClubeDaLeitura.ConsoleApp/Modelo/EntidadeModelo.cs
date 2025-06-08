814e9871c3bcc76ab3d51834cc8c3816052f8c60
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-06-08 16:08:56 -0300

    Criado classe EntidadeModelo com seus respectivos métodos e propriedades.

---
+++ ClubeDaLeitura.ConsoleApp/Modelo/EntidadeModelo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDaLeitura.ConsoleApp.Modelo
+{
+    public abstract class EntidadeModelo
+    {
+        public int id;
+
+        public abstract void atualizaRegistro(EntidadeModelo registro);
+        public abstract string validar();
+    }
+}
