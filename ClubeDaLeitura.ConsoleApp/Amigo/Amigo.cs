ff49a43f880f3539233f64a86f2b55dd12c1659d
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-06-10 02:50:57 -0300

    Criado classe Amigo com seus respectivos atributos e o método AtualizarRegistro()

---
+++ ClubeDaLeitura.ConsoleApp/Amigo/Amigo.cs
@@ -0,0 +1,30 @@
+﻿using ClubeDaLeitura.ConsoleApp.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDaLeitura.ConsoleApp.Amigo
+{
+    public class Amigo : EntidadeModelo
+    {
+        public string Nome;
+        public string Responsavel;
+        public string Telefone;
+
+        public override string ValidarDados()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AtualizarRegistro(EntidadeModelo registro)
+        {
+            Amigo amigoAtualizado = (Amigo)registro;
+
+            this.Nome = amigoAtualizado.Nome;
+            this.Responsavel = amigoAtualizado.Responsavel;
+            this.Telefone = amigoAtualizado.Telefone;
+        }
+    }
+}
