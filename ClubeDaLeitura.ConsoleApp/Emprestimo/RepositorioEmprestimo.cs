7786a52ba760a72f45993365e8fb9b036453190d
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-06-10 17:02:17 -0300

    Criado classe RepositorioEmprestimo com seu método SelecionarEmprestimosAtivos().

---
+++ ClubeDaLeitura.ConsoleApp/Emprestimo/RepositorioEmprestimo.cs
@@ -0,0 +1,29 @@
+﻿using ClubeDaLeitura.ConsoleApp.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDaLeitura.ConsoleApp.Emprestimo
+{
+    public class RepositorioEmprestimo : RepositorioModelo
+    {
+        public List<Emprestimo> SelecionarEmprestimosAtivos()
+        {
+            List<Emprestimo> emprestimosAtivos = new List<Emprestimo>();
+
+            List<EntidadeModelo> registros = PegarRegistros();
+
+            foreach (EntidadeModelo rd in registros)
+            {
+                Emprestimo emprestimo = (Emprestimo)rd;
+
+                if (emprestimo.Status == "Aberto" || emprestimo.Status == "Atrasado")
+                    emprestimosAtivos.Add(emprestimo);
+            }
+
+            return emprestimosAtivos;
+        }
+    }
+}
