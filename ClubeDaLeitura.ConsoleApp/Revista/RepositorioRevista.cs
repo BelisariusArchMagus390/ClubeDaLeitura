e216f38fd298ff9ba37af27fe21653dd006cd128
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-06-10 16:05:45 -0300

    Criado classe RepositorioRevista com seu respectivo método SelecionarRevistasDisponiveis()

---
+++ ClubeDaLeitura.ConsoleApp/Revista/RepositorioRevista.cs
@@ -0,0 +1,30 @@
+﻿using ClubeDaLeitura.ConsoleApp.Modelo;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDaLeitura.ConsoleApp.Revista
+{
+    public class RepositorioRevista : RepositorioModelo
+    {
+        public List<Revista> SelecionarRevistasDisponiveis()
+        {
+            List<Revista> revistasDisponiveis = new List<Revista>();
+
+            List<EntidadeModelo> registros = PegarRegistros();
+
+            foreach (EntidadeModelo rd in registros)
+            {
+                Revista revista = (Revista)rd;
+
+                if (revista.Status == "Disponivel")
+                    revistasDisponiveis.Add(revista);
+            }
+
+            return revistasDisponiveis;
+        }
+    }
+}
