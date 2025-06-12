5cef0db4b61744ae9d5f8adb9f308dea04888c68
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-06-12 19:32:41 -0300

    Criado classe RepositorioReservas.

---
+++ ClubeDaLeitura.ConsoleApp/ModuloReservas/RepositorioReservas.cs
@@ -0,0 +1,11 @@
+﻿using ClubeDaLeitura.ConsoleApp.ModuloModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDaLeitura.ConsoleApp.ModuloReservas
+{
+    internal class RepositorioReservas : RepositorioModelo;
+}
